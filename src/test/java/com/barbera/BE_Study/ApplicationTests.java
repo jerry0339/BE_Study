f255d0701ad51ea40af3ae05d828891fd92b3c41
전찬의 <jcu011@naver.com>
2022-06-21 20:11:02 +0900

    chore : init Project

---
+++ src/test/java/com/barbera/BE_Study/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbera.BE_Study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
