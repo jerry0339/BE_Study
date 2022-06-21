d84d819c015221f49e862b3085635ca0792622de
전찬의 <jcu011@naver.com>
2022-06-21 20:43:55 +0900

    feat : test용 Post Controller 추가

---
+++ src/main/java/com/barbera/bestudy/Post.java
@@ -0,0 +1,18 @@
+package com.barbera.bestudy;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import lombok.Data;
+
+@Data
+@Entity(name = "post")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "post_id_seq")
+    @SequenceGenerator(name="post_id_seq", sequenceName = "post_id_seq", allocationSize = 1)
+    private Long id;
+    private String content;
+}
