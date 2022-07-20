6454ee670d2054508591af334322df42ca961612
전찬의 <jcu011@naver.com>
2022-07-20 17:27:39 +0900

    fix : DataSource 없어서 임시 수정

--- src/main/java/com/barbera/bestudy/domain/PostRepository.java
+++ src/main/java/com/barbera/bestudy/domain/PostRepository.java
@@ -1,9 +1,9 @@
-package com.barbera.bestudy.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-
-}
+//package com.barbera.bestudy.domain;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface PostRepository extends JpaRepository<Post, Long> {
+//
+//}
