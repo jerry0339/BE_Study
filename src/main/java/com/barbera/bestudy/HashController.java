ce60a2ac53132a0e662a8b1b5f701a687da609f2
전찬의 <jcu011@naver.com>
2022-07-25 11:25:11 +0900

    chore : jenkins lsof 설정 테스트

--- src/main/java/com/barbera/bestudy/HashController.java
+++ src/main/java/com/barbera/bestudy/HashController.java
@@ -21,7 +21,7 @@ public class HashController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello";
+        return "Updated hello";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
